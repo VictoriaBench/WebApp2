215784dce60c129ac1d04df30bc03bf792198adb
VictoriaBench <victoria.bench@gmail.com>
2021-03-25 16:51:43 +0200

    first commit

---
+++ mainRoutes.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const express = require('express')
+const mainRouter = express.Router()
+
+mainRouter.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+mainRouter.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+module.exports = mainRouter
