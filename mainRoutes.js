719c117b2c2af7ed7a7a429ee91dfbb0a347493b
VictoriaBench <victoria.bench@gmail.com>
2021-03-25 18:42:55 +0200

    node added to hello world

--- mainRoutes.js
+++ mainRoutes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const mainRouter = express.Router()
 
 mainRouter.get('/', function (req, res) {
-  res.send('hello world')
+  res.send('hello world I\'m node!')
 })
 
 mainRouter.get('/about', function (req, res) {
